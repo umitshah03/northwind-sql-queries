9195f37a90dafa6e9b6e852b9357106d17eabad0
umitshah03 <102597862+umitshah03@users.noreply.github.com>
2024-03-05 13:21:23 +0530

    The top 5 employees who have processed the most orders

--- QUESTION2.sql
+++ QUESTION2.sql
@@ -6,5 +6,5 @@ INNER JOIN
     products ON suppliers.supplier_id = products.supplier_id
 INNER JOIN 
     categories ON products.category_id = categories.category_id
-WHERE 
+WHERE
     categories.category_name = 'Seafood';
