d595aef7925445eb82ee4d4c15f472009861013e
umitshah03 <102597862+umitshah03@users.noreply.github.com>
2024-03-05 13:23:12 +0530

    All suppliers who provide products in the ‘Seafood’ category

--- QUESTION2.sql
+++ QUESTION2.sql
@@ -2,7 +2,7 @@ select category_name, description from categories
 order by category_nameSELECT company_name
 FROM 
     suppliers 
-INNER JOIN 
+INNER JOIN
     products ON suppliers.supplier_id = products.supplier_id
 INNER JOIN 
     categories ON products.category_id = categories.category_id
